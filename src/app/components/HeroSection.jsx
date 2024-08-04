18726e053b172540dcab8d497322e38872fd008c
Dongseung <ttjh92@hanmail.net>
2024-08-04 19:03:50 +0900

    Update hero section text and adjust mobile layout for image centering

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -8,7 +8,7 @@ import { motion } from "framer-motion";
 const HeroSection = () => {
   return (
     <section className="lg:py-16">
-      <div className="grid gird-cols-1 sm:grid-cols-12">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -38,7 +38,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="mb-6 text-[#adb7be] text-base sm:text-lg lg:text-xl word-break-keep-all">
-          저는 동적인 사용자 친화적인 웹사이트와 모바일 애플리케이션을 만드는 열정적인 개발자입니다. 아이디어를 혁신적인 디지털 솔루션으로 변환하는 데 주력하고 있습니다. 함께 협력하여 당신의 비전을 현실로 만들어 봅시다!
+          안녕하세요! 저는 사용자 친화적인 웹사이트와 모바일 애플리케이션을 만드는 데 열정을 가진 개발자입니다. 아이디어를 혁신적인 디지털 솔루션으로 바꾸는 것을 좋아하며, 함께 협력하여 여러분의 비전을 현실로 만드는 데 최선을 다하겠습니다. 함께 멋진 프로젝트를 만들고 싶습니다!
           </p>
           <div>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-200 text-white">
@@ -55,7 +55,7 @@ const HeroSection = () => {
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-4 place-self-center mt-4 lg:mt-0"
+          className="col-span-4 place-self-center sm:justify-self-start mt-8 sm:mt-0 lg:mt-0"
         >
           <div className="rounded-full bg-[#181818] w-[230px] h-[230px] lg:w-[380px] lg:h-[380px] relative">
             <Image
